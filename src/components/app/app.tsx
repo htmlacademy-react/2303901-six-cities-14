ebaa25d02f7a01edb4915b99eeff09212fd00f52
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2023-10-17 21:49:33 +0400

    Маршрут по тайным тропам #3
    * выполнил задание 3.13. Маршрут по тайным тропам
    * исправил замечания по проверке в гит хаб

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,50 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import MainPages from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import ErrorMessage from '../error-message/error-message';
+import PrivateRoute from '../private-route/private-route';
+import {AppRoute, AuthorizationStatus} from '../../const';
 
 type AppOfferProps = {
-  offerCounts: number;
+  CountOffers: number;
 }
 
-function App({offerCounts}: AppOfferProps): JSX.Element {
+function App({CountOffers: countOffers}: AppOfferProps): JSX.Element {
   return (
-    <MainPages offerCounts={offerCounts} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element ={<MainPages CountOffers = {countOffers}/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element ={<LoginPage/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element ={
+            <PrivateRoute authorizationStatus = {AuthorizationStatus.NoAuth}>
+              <FavoritesPage/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Offer}
+          element ={<OfferPage/>}
+        />
+        <Route
+          path={AppRoute.Error}
+          element ={<ErrorMessage/>}
+        />
+        <Route
+          path="*"
+          element={<ErrorMessage/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App offerCounts = {Offers.CountOffers}/>
+    <App CountOffers = {Offers.CountOffers}/>
   </React.StrictMode>
 );
