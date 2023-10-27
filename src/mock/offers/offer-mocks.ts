3690831d8f3d18de00a27044be11bac4931a3320
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2023-10-27 12:58:22 +0400

    подправил отрисовку фаворитных предложенрий

--- src/mock/offers/offer-mocks.ts
+++ src/mock/offers/offer-mocks.ts
@@ -239,7 +239,7 @@ const offersMock: Offers = [
       'https://14.react.pages.academy/static/offer/5.jpg'
     ],
     title: 'Nice, cozy, warm big bed apartment',
-    isFavorite: false,
+    isFavorite: true,
     isPremium: false,
     rating: 3.2,
     type: 'hotel',
