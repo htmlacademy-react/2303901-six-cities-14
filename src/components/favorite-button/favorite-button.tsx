69fc78d6e786fbdf3d6b67e372f3793251a5ab27
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2023-11-05 10:25:55 +0400

    Реализовал добавление и удаление фаворитных карточек предложения

--- src/components/favorite-button/favorite-button.tsx
+++ src/components/favorite-button/favorite-button.tsx
@@ -1,38 +1,42 @@
-import {useState} from 'react';
-import type {Offer} from '../../mock/offers/offer-mocks';
-// import {useDispatch, useSelector} from 'react-redux';
-// import { addOfferList } from '../../store/actions/action';
+import type { Offer } from '../../mock/offers/offer-mocks';
+import { useDispatch, useSelector } from 'react-redux';
+import { addOfferList } from '../../store/actions/action';
+import type { Offers } from '../../mock/offers/offer-mocks';
+
+type StateOffers = {
+  offers: Offers;
+}
 
 type ButtonProps = {
   offer: Offer;
-}
+};
 
-function FavoriteButton ({offer}: ButtonProps): JSX.Element {
+function FavoriteButton({ offer }: ButtonProps): JSX.Element {
 
-  // const dispatch = useDispatch();
-  // const stateOffers = useSelector((state) => state.offers)
-  // const indexOffer = stateOffers.findIndex((stateOffer) => stateOffer.id === offer.id);
-
-  const [isFavoriteCard, setIsFavoriteCard] = useState(offer.isFavorite);
+  const dispatch = useDispatch();
+  const stateOffers = useSelector((state: StateOffers) => state.offers);
+  const indexOffer = stateOffers.findIndex((stateOffer) => stateOffer.id === offer.id);
+  const updatedStateOffers = [...stateOffers];
 
   const onFavoriteButton = (): void => {
-    const updatedIsFavorite = !isFavoriteCard;
+
+    const updatedIsFavorite = !offer.isFavorite;
 
     const updatedOffer = {
       ...offer,
       isFavorite: updatedIsFavorite
     };
 
-    //const changeOffer = Array.from(stateOffers.splice(indexOffer, 0, updatedOffer));
-    //console.log(changeOffer)
-    // console.log(stateOffers)
-    //dispatch(addOfferList(changeOffer));
-    setIsFavoriteCard(updatedOffer.isFavorite);
+    updatedStateOffers[indexOffer] = updatedOffer;
+    dispatch(addOfferList(updatedStateOffers));
   };
 
   return (
-
-    <button onClick={onFavoriteButton} className={`place-card__bookmark-button ${isFavoriteCard ? 'place-card__bookmark-button--active' : ''} button`} type="button">
+    <button
+      onClick={onFavoriteButton}
+      className={`place-card__bookmark-button ${offer.isFavorite ? 'place-card__bookmark-button--active' : ''} button`}
+      type="button"
+    >
       <svg className="place-card__bookmark-icon" width="18" height="19">
         <use xlinkHref="#icon-bookmark"></use>
       </svg>
