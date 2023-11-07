3d0d6749c00e1b25610d96a7d0f34ebd5bbbd6fc
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2023-11-07 11:37:08 +0400

    WIP создал новый компонет сортировки и реализовал логику сортировки офферов

---
+++ src/components/sort-list/sort-list.tsx
@@ -0,0 +1,72 @@
+import {useEffect, useState} from 'react';
+import type {Offers} from '../../mock/offers/offer-mocks';
+
+type OffersSort = {
+  offers: Offers;
+}
+
+function SortList ({offers}: OffersSort) {
+  const [stateSortList, setStateSortList] = useState(false);
+  const [stateSortOffers, setStateSortOffers] = useState(offers);
+
+  useEffect(()=>{
+    setStateSortList(false);
+  },[offers]);
+
+  function changeOfferLowToHight (offersToSort: Offers) {
+    setStateSortOffers([...offersToSort].sort((a, b) => a.price - b.price));
+  }
+
+  function changeOfferHightToLow (offersToSort: Offers) {
+    setStateSortOffers([...offersToSort].sort((a, b) => b.price - a.price));
+  }
+
+  function changeOfferTopRatedFirst (offersToSort: Offers) {
+    setStateSortOffers([...offersToSort].sort((a, b) => b.rating - a.rating));
+  }
+
+  function onClickSort():void {
+    setStateSortList(!stateSortList);
+  }
+
+  function onClickChangeSort(evt: React.MouseEvent<HTMLLIElement>):void {
+
+    switch(evt.currentTarget.tabIndex) {
+      case 0:
+        return setStateSortOffers(offers);
+      case 1:
+        return changeOfferLowToHight(offers);
+      case 2:
+        return changeOfferHightToLow(offers);
+      case 3:
+        return changeOfferTopRatedFirst(offers);
+      default:
+        return setStateSortOffers(offers);
+    }
+  }
+  //console.table(stateSortOffers)
+  return (
+    <form className="places__sorting" action="#" method="get" >
+      <span className="places__sorting-caption">Sort by</span>
+      <span className="places__sorting-type" tabIndex={0} onClick={onClickSort}>
+        Popular
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      <ul
+        className={`places__options places__options--custom
+          stateSortList
+          ${stateSortList ? 'places__options--opened' : ''}`}
+        onClick={onClickChangeSort}
+      >
+        <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+        <li className="places__option" tabIndex={1}>Price: low to high</li>
+        <li className="places__option" tabIndex={2}>Price: high to low</li>
+        <li className="places__option" tabIndex={3}>Top rated first</li>
+      </ul>
+    </form>
+  );
+}
+
+export {SortList};
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,12 +1,13 @@
+import {useSelector} from 'react-redux';
+import {useState} from 'react';
 import ListOffers from '../../components/list-offers/list-offers';
 import MapComponent from '../../components/map/map';
 import FilterCities from '../../components/filter-cities/filter-cities';
 import type {Offer} from '../../mock/offers/offer-mocks';
 import useDocumentTitle from '../../hooks/document-title';
-import {useState} from 'react';
 import Profile from '../../components/profile/profile';
-import {useSelector} from 'react-redux';
 import type {StateFilterCity, StateOffers} from '../../types/type-store';
+import { SortList } from '../../components/sort-list/sort-list';
 
 type MainPagesProps = {
   title: string;
@@ -82,22 +83,8 @@ function MainPages ({title}: MainPagesProps): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found"> {citiesToFilter.length} places to stay in {selectedFilterCity}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
 
+              <SortList offers = {citiesToFilter}/>
               <ListOffers offers = {citiesToFilter} handleIdOffer = {handleListItemHover} onLeaveMouseOffer={onLeaveMouseOffer}/>
 
             </section>
