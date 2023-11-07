cf8f3d2acf53bf46aa8bfa66311a7cfbdd1d26f1
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2023-11-07 19:33:26 +0400

    Скрыл выпадающий список сортировки когда нет офферов

--- src/components/sort-list/sort-list.tsx
+++ src/components/sort-list/sort-list.tsx
@@ -9,20 +9,16 @@ function SortList () {
 
   const offersSort = useSelector((state: StateOffersSort) => state.sortOffers.sortOffers);
   const offersCity = useSelector((state: StateOffersFilter) => state.filterOffers.filterOffers);
-
   const dispatch = useDispatch();
 
-  //console.log(offersCity.filterOffers.filterOffers);
-  useEffect(()=>{
+  useEffect(() => {
     setStateSortList(false);
-    //dispatch(sortOffersSlice.actions.addSortOffers(offers));
   },[offersCity]);
 
   function changeOfferLowToHight (offersToSort: Offers) {
     const offers = [...offersToSort].sort((a, b) => a.price - b.price);
 
     dispatch(sortOffersSlice.actions.addSortOffers(offers));
-
   }
 
   function changeOfferHightToLow (offersToSort: Offers) {
@@ -59,26 +55,29 @@ function SortList () {
   }
 
   return (
-    <form className="places__sorting" action="#" method="get" >
-      <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0} onClick={onClickSort}>
-        Popular
-        <svg className="places__sorting-arrow" width="7" height="4">
-          <use xlinkHref="#icon-arrow-select"></use>
-        </svg>
-      </span>
-      <ul
-        className={`places__options places__options--custom
-          stateSortList
-          ${stateSortList ? 'places__options--opened' : ''}`}
-        onClick={onClickChangeSort}
-      >
-        <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-        <li className="places__option" tabIndex={1}>Price: low to high</li>
-        <li className="places__option" tabIndex={2}>Price: high to low</li>
-        <li className="places__option" tabIndex={3}>Top rated first</li>
-      </ul>
-    </form>
+    offersCity.length ? (
+      <form className="places__sorting" action="#" method="get">
+        <span className="places__sorting-caption">Sort by</span>
+        <span className="places__sorting-type" tabIndex={0} onClick={onClickSort}>
+          Popular
+          <svg className="places__sorting-arrow" width="7" height="4">
+            <use xlinkHref="#icon-arrow-select"></use>
+          </svg>
+        </span>
+        <ul
+          className={`places__options places__options--custom
+            stateSortList
+            ${stateSortList ? 'places__options--opened' : ''}`}
+          onClick={onClickChangeSort}
+        >
+          <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+          <li className="places__option" tabIndex={1}>Price: low to high</li>
+          <li className="places__option" tabIndex={2}>Price: high to low</li>
+          <li className="places__option" tabIndex={3}>Top rated first</li>
+        </ul>
+      </form>
+    ) : ''
+
   );
 }
 
