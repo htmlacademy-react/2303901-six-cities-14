2ef96d4d762c22e162ba66d0ebf5679a8413e93b
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2023-11-16 10:45:45 +0400

    отсортировал комменты

--- src/components/list-review/list-review.tsx
+++ src/components/list-review/list-review.tsx
@@ -2,18 +2,27 @@ import Review from '../review/review';
 import {MAX_LENGTH_REVIEW} from '../../const';
 import {useAppSelector} from '../../hooks/use-store';
 import {useEffect} from 'react';
+import type { Comment } from '../../types/type-store';
 
 function ListReview () {
   const comments = useAppSelector((state) => state.loadComments.comments);
   const reviews = comments?.slice(0, MAX_LENGTH_REVIEW);
 
   useEffect(() => {
-
   }, [comments, reviews]);
+
+
+  function compareDates(a: Comment, b: Comment): number {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateB.getTime() - dateA.getTime();
+  }
+
+  const sort = reviews?.sort(compareDates);
 
   return(
     <ul className="reviews__list">
-      {reviews?.map((review) => <Review key={review.id} reviewProps={review}/>)}
+      {sort?.map((review) => <Review key={review.id} reviewProps={review}/>)}
     </ul>
   );
 }
